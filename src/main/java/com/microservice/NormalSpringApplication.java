d3d4639a642bbec357fb803e782243ffc234a6f8
upendergupta <upender.gupta1991@gmail.com>
2022-07-08 21:50:50 +0530

    First

---
+++ src/main/java/com/microservice/NormalSpringApplication.java
@@ -0,0 +1,13 @@
+package com.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NormalSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NormalSpringApplication.class, args);
+	}
+
+}
