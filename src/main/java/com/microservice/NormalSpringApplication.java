b356b68ab6159533ab9050953acc0fb0accaf256
upendergupta <upender.gupta1991@gmail.com>
2022-07-09 19:04:24 +0530

    Thrid commit

--- src/main/java/com/microservice/NormalSpringApplication.java
+++ src/main/java/com/microservice/NormalSpringApplication.java
@@ -1,14 +1,21 @@
 package com.microservice;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NormalSpringApplication {
-
+	public static Logger log=LoggerFactory.getLogger(NormalSpringApplication.class);
+	@PostConstruct
+	public void ram(){
+		log.info("Application Started......");
+	}
 	public static void main(String[] args) {
-		
-		System.out.println("Upender");
+		log.info("Application executed......");
 		SpringApplication.run(NormalSpringApplication.class, args);
 	}
 
--- src/test/java/com/microservice/NormalSpringApplicationTests.java
+++ src/test/java/com/microservice/NormalSpringApplicationTests.java
@@ -1,13 +1,19 @@
 package com.microservice;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NormalSpringApplicationTests {
+	
+	public static Logger log=LoggerFactory.getLogger(NormalSpringApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		log.info("Test Executed Successfully..................");
+		//assertEquals();
 	}
 
 }
